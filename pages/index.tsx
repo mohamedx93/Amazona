6048261db16a5fd7ebf0fee84ab8c1452af8f99a
mabnf93 <mabnf93@gmail.com>
2021-11-09 21:40:56 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import Layout from '../components/Layout';
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
