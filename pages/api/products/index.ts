eacc15b3b8a6f707732fc65101657eb411735b5c
mabnf93 <mabnf93@gmail.com>
2021-11-28 22:16:13 +0100

    commit 28-11-2021--next_connect

---
+++ pages/api/products/index.ts
@@ -0,0 +1,15 @@
+import nc from 'next-connect';
+import db from '../../../utils/db';
+import Product from '../../../models/Product';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = nc();
+
+handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
+  await db.connect();
+  const products = await Product.find({});
+  db.disconnect();
+  res.send(products);
+});
+
+export default handler;
